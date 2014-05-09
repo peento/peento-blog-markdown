9811f548d055c43a08441c3162f9fdcc5a325e74
Zongmin Lei <leizongmin@gmail.com>
2014-05-09 21:31:09 +0800

    暂停

---
+++ src/hook/article.js
@@ -0,0 +1,12 @@
+/**
+ * Hook: article
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+module.exports = function (ns, register, debug) {
+  var markdown = require('markdown');
+
+  
+
+};
